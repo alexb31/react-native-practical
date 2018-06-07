c8cb7b5cc369e089844f40569b3b74a553154eec
alexb31 <alexandrebornstein@gmail.com>
2018-06-08 00:19:44 +0200

    Setting up Redux, Creating Store & setting up Actions & Reducers

--- my-first-app/index.js
+++ my-first-app/index.js
@@ -1,3 +1,14 @@
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
-AppRegistry.registerComponent('myfirstapp', () => App);
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
+
+const RNRedux = () => (
+  <Provider store={store} >
+    <App />
+  </Provider>
+)
+
+AppRegistry.registerComponent('myfirstapp', () => RNRedux);
