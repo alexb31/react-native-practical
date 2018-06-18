dabe825c6f15d421539360fa5cd0af9671bbcb67
alexb31 <alexandrebornstein@gmail.com>
2018-06-18 19:30:59 +0200

    Styled placeDetail

--- my-first-app/src/screens/SharePlace/SharePlace.js
+++ my-first-app/src/screens/SharePlace/SharePlace.js
@@ -10,6 +10,10 @@ import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import PickLocation from '../../components/PickLocation/PickLocation';
 
 class SharePlaceScreen extends Component {
+    static navigatorStyle = {
+        navBarButtonColor: "orange"
+    }
+    
     state = {
         placeName: ""
     }
