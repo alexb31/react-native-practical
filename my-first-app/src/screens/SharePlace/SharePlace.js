dcd61940844a774b084ef336b2dd64a5cb42d821
alexb31 <alexandrebornstein@gmail.com>
2018-06-27 00:22:57 +0200

    Adding Activity Indicator

--- my-first-app/src/screens/SharePlace/SharePlace.js
+++ my-first-app/src/screens/SharePlace/SharePlace.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, ScrollView, Image } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView, Image, ActivityIndicator } from 'react-native';
 import { connect } from 'react-redux';
 
 import { addPlace } from '../../store/actions/index';
@@ -103,6 +103,16 @@ class SharePlaceScreen extends Component {
       };
 
     render () {
+        let submitButton = (<Button title="Share The Place" onPress={this.placeAddedHandler} disabled={
+            !this.state.controls.placeName.valid || 
+            !this.state.controls.location.valid ||
+            !this.state.controls.image.valid
+            }/>);
+
+        if (this.props.isLoading) {
+            submitButton = <ActivityIndicator/>;
+        }
+
         return (
             <ScrollView>
             <View style={styles.container}>
@@ -113,11 +123,7 @@ class SharePlaceScreen extends Component {
                 <PickLocation onLocationPick={this.locationPickedHandler}/>
                 <PlaceInput placeData={this.state.controls.placeName} onChangeText={this.placeNameChangedHandler}/>
                 <View style={styles.button}>
-                <Button title="Share The Place" onPress={this.placeAddedHandler} disabled={
-                    !this.state.controls.placeName.valid || 
-                    !this.state.controls.location.valid ||
-                    !this.state.controls.image.valid
-                    }/>
+                    {submitButton}
                 </View>
             </View>
             </ScrollView>
@@ -146,10 +152,16 @@ const styles = StyleSheet.create({
     }
 });
 
+const mapStateToProps = state => {
+    return {
+      isLoading: state.ui.isLoading
+    };
+  };
+
 const mapDispatchToProps = dispatch => {
     return {
       onAddPlace: (placeName, location, image) => dispatch(addPlace(placeName, location, image))
     };
   };  
 
-export default connect(null, mapDispatchToProps)(SharePlaceScreen);
+export default connect(mapStateToProps, mapDispatchToProps)(SharePlaceScreen);
--- my-first-app/src/store/actions/places.js
+++ my-first-app/src/store/actions/places.js
@@ -1,31 +1,40 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
+import { uiStartLoading, uiStopLoading } from './index';
 
 export const addPlace = (placeName, location, image) => {
     return dispatch => {
-        const placeData = {
-            name: placeName,
-            location: location
-        };
+        dispatch(uiStartLoading());
         fetch("https://us-central1-awesome-places-3e57b.cloudfunctions.net/storeImage", {
             method: "POST",
             body: JSON.stringify({
                 image: image.base64
             })
         })
-        .catch(err => console.log(err))
+        .catch(err => {
+            console.log(err);
+            dispatch(uiStopLoading());
+        })
+        .then(res => res.json())
+        .then(parsedRes => {
+            const placeData = {
+                name: placeName,
+                location: location,
+                image: parsedRes.imageUrl
+            };
+            return fetch("https://awesome-places-3e57b.firebaseio.com/places.json", {
+                method: "POST",
+                body: JSON.stringify(placeData)
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch(uiStopLoading());
+        })
         .then(res => res.json())
         .then(parsedRes => {
             console.log(parsedRes);
+            dispatch(uiStopLoading());
         });
-        // fetch("https://awesome-places-3e57b.firebaseio.com/places.json", {
-        //     method: "POST",
-        //     body: JSON.stringify(placeData)
-        // })
-        // .catch(err => console.log(err))
-        // .then(res => res.json())
-        // .then(parsedRes => {
-        //     console.log(parsedRes);
-        // });
     };
 };
 
