7b6dc7463eaee943abcd4ee010f80debafe45aea
alexb31 <alexandrebornstein@gmail.com>
2018-07-02 19:43:10 +0200

    Signins Users Up without URL API :/

--- my-first-app/src/store/actions/auth.js
+++ my-first-app/src/store/actions/auth.js
@@ -1,8 +1,31 @@
 import { TRY_AUTH } from './actionTypes';
 
 export const tryAuth = (authData) => {
-  return {
-    type: TRY_AUTH,
-    authDate: authData
-  }
-}
+  return dispatch => {
+    dispatch(authSignup(authData));
+  };
+};
+
+export const authSignup = (authData) => {
+  return dispatch => {
+    fetch("API_LINK", {
+      method: "POST",
+      body: JSON.stringify({
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      alert("Authentication failed");
+    })
+    .then(res => res.json())
+    .then(parsedRes => {
+      console.log(parsedRes);
+    })
+  };
+};
